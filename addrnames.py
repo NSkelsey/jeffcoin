9cbe3835307177ef4f04fb8bcf69e05b859b5e8c
Ubuntu <ubuntu@ip-10-65-29-56.ec2.internal>
2014-04-29 00:24:42 +0000

    Address name parsing works well enough

--- addrnames.py
+++ addrnames.py
@@ -2,6 +2,7 @@ from binascii import hexlify
 from hashlib import sha256
 
 import bitcoin
+from bitcoin.wallet import CBitcoinAddress
 from bitcoin.base58 import encode, decode
 from pycoin.encoding import b2a_base58, a2b_base58
 
@@ -12,10 +13,10 @@ def conv_int(num):
     return int(out)
 
 def make_addr(num):
-    assert num < 2**160
+    assert num < 2**192
     t = num
-    while t < 2**160:
-        if t*58 > 2 ** 160:
+    while t < 2**192:
+        if t*58 > 2 ** 192:
             break
         t = t * 58 + 30
 
@@ -25,9 +26,10 @@ def make_addr(num):
     chksm = bitcoin.core.Hash(chnk)[:4]
     #chksm = sha256(sha256(t.to_bytes(160, 'big')).digest()).digest()[:4]
     i = int(hexlify(chksm), 16)
+
     o = t*(2**32) + i
 
-    return int(o)
+    return o
 
 
 def from_str(s):
@@ -36,12 +38,13 @@ def from_str(s):
     num = int(hexlify(decode(s)), 16)
     out = make_addr(num)
 
-    out = encode(b'\x00' + out.to_bytes(20, 'big'))
+    out = encode(b'\x00' + out.to_bytes(24, 'big'))
     return out 
     
 
 if __name__ == "__main__":
-    s = "hashtagWAGWAG"
+    s = "Counterparty"
     
-    from_str(s)
-    print(from_str(s))
+    addr = from_str(s)
+    print(addr)
+    print(CBitcoinAddress(addr))
